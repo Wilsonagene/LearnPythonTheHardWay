de744ee912108ccfaae41ef4c98ce56271e6d8e6
Uche evaristus <ucheeva15@gmail.com>
2025-12-11 00:48:23 -0800

    Uche work commit

---
+++ wilsonexcesise1-3.py
@@ -0,0 +1,35 @@
+#Exercise 0 was about installlation of python, . Anaconda, . Jupyter, . Geany, Testing setup and some CMD commands.
+#Exercise 1: Write a Python program to print Statement e.g "Hello World
+print("Hello World") #this line prints Hello World to the console
+print("Hello Again") #this line prints Hello Again to the console
+print("i like typing this.") #this line prints i like typing this. to the console
+print("This is fun.") #this line prints This is fun. to the console
+print('Yay! Printing.') #this line prints Yay! Printing. to the console
+print("I'd much rather you 'not'.") #this line prints I'd much rather you 'not'. to the console
+print('I "said" do not touch this.') #this line prints I "said" do not touch this. to the console
+
+
+#Exercise 2. Comments and Pound Characters
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+print("I could have code like this.") # and the comment after is ignored
+# You can also use a comment to "disable" or comment out a piece of code:
+# print("This won't run.")
+print("This will run.") # this line prints This will run. to the console
+
+#Exercise 3: Numbers and Math
+#this excercise demonstrates basic arithmetic operations in Python like + plus, - minus, / slash, * asterisk and so on.
+print("I will now count my chickens:") #this line prints I will now count my chickens: to the console
+print("Hens", 25 + 30 / 6) #this line prints Hens 30.0 to the console
+print("Roosters", 100 - 25 * 3 % 4) #this line prints Roosters 97 to the console
+print("Now I will count the eggs:") #this line prints Now I will count the eggs: to the console
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #this line prints 6.75 to the console
+print("Is it true that 3 + 2 < 5 - 7?") #this line prints Is it true that 3 + 2 < 5 - 7? to the console
+print(3 + 2 < 5 - 7) #this line prints False to the console
+print("What is 3 + 2?", 3 + 2) #this line prints What is 3 + 2? 5 to the console
+print("What is  5 - 7?", 5 - 7) #this line prints What is 5 - 7? -2 to the console
+print("Oh, that's why it's False.") #this line prints Oh, that's why it's False. to the console
+print("How about some more.") #this line prints How about some more
+print("Is it greater?", 5 > -2) #this line prints Is it greater? True to the console
+print("Is it greater or equal?", 5 >= -2) #this line prints Is it greater or equal? True to the console
+print("Is it less or equal?", 5 <= -2) #this line prints Is it less or equal? False to the console
